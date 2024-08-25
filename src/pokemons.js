c164fbf31572fe9d9c6032f6ce236e66742d98ec
AngeloKwakye <dangelokwakye@gmail.com>
2024-08-25 07:20:17 +0000

    Pokémon dummy data

---
+++ src/pokemons.js
@@ -0,0 +1,9 @@
+
+export const Pokemons = [
+    {name: 'Ivysaur', usdtPrice: 71, image: 'https://archives.bulbagarden.net/media/upload/8/81/0002Ivysaur.png'},
+    {name: 'Toxtricity', usdtPrice: 42, image: 'https://archives.bulbagarden.net/media/upload/7/79/0849Toxtricity.png'},
+    {name: 'Eevee', usdtPrice: 87, image: 'https://archives.bulbagarden.net/media/upload/4/4c/0133Eevee.png'},
+    {name: 'Squirtle', usdtPrice: 19, image: 'https://archives.bulbagarden.net/media/upload/5/54/0007Squirtle.png'},
+    {name: 'Blastoise', usdtPrice: 22, image: 'https://archives.bulbagarden.net/media/upload/0/0a/0009Blastoise.png'},
+    {name: 'Gengar', usdtPrice: 63, image: 'https://archives.bulbagarden.net/media/upload/4/47/0094Gengar.png'}
+]
